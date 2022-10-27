b981dd81895bcd04fa7f09c3b6a0629db38578ad
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-27 21:18:03 +1400

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,41 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        
+        int vow = 0;
+        int con = 0;
+
+        Character[] vowel = {'e', 'y', 'u', 'i', 'o', 'a'};
+
+        Character[] consonant = {'q', 'w', 'r', 't', 'p', 's',
+                                 'd', 'f', 'g', 'h', 'j', 'k',
+                                 'l', 'z', 'x', 'c', 'v', 'b',
+                                 'n', 'm'};
+
+        for (int i = 0; i < vowel.length; i++) {
+            char uppercaseVowel = Character.toUpperCase(vowel[i]);
+            if (character == vowel[i] || character == uppercaseVowel) {
+                vow++;
+            }
+
+            else {
+                for (int j = 0; j < consonant.length; j++) {
+                    char uppercaseConsonant = Character.toUpperCase(consonant[j]);
+                    if (character == consonant[j] || character == uppercaseConsonant) {
+                        con++;
+                    }
+                }
+            }
+        }
+
+        if (vow > 0) {
+            return "Vowel";
+        }
+        else if (con > 0) {
+            return "Consonant";
+        }
+        else {
+            return "wrong alphabet!";
+        }
     }
 }
