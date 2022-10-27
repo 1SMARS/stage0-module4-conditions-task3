ddcf87a83e5c72575d34b766e88cf1b1c035a2ea
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-27 20:12:52 +1400

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -3,12 +3,22 @@ package school.mjc.stage0.conditions.task3;
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
         
-        if (symbol == 'r') {
-            System.out.println("English");
-        } else if (symbol == 'R') {
-            System.out.println("English");
-        } else {
-            System.out.println("Non English");
-        }
+         int s = 0;
+        Character[] array = {'q','w','e','r','t','y','u','i','o','p','l','k',
+                        'j','h','g','f','d','s','a','z','x','c','v','b','n','m',
+                       'Q','W','E','R','T','Y','U','I','O','P','A','S',
+                       'D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+
+       for (int i = 0; i < array.length; i++) {
+
+           if (symbol == array[i]) {
+               s++;
+           }
+       }
+
+       if (s > 0) {
+           System.out.println("English");
+       } else
+           System.out.println("Non English");
     }
 }
