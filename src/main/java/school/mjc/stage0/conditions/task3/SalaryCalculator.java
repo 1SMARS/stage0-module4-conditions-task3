90143a9993f68982fbf1894a921eff26506d4281
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-27 20:39:52 +1400

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        
+        double fifteenTaxes = .15;
+        double eighteenTaxes = .18;
+        double twentyTaxes = .20;
+
+//        int salary = 5000;
+
+
+
+        if (salary <= 10000 && salary > 0) {
+            return salary - (salary * fifteenTaxes);
+        } else if (salary > 10000 && salary <= 20000) {
+            return salary - (salary * eighteenTaxes);
+        } else if (salary > 20000) {
+            return salary - (salary * twentyTaxes);
+
+        } else if (salary < 0) {
+            Sreturn "wrong input!";
+        }
+        else {
+            return false;
+        }
     }
 }
