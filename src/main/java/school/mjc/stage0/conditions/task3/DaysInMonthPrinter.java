09c5137bf294146671c0029c6c75cd59c45788c2
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-27 21:34:36 +1400

    Update DaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -3,32 +3,45 @@ package school.mjc.stage0.conditions.task3;
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
         
-        if (month == 1) {
-            System.out.println("January");
-        } else if (month == 2) {
-            System.out.println("February");
-        } else if (month == 3) {
-            System.out.println("March");
-        } else if (month == 4) {
-            System.out.println("April");
-        } else if (month == 5) {
-            System.out.println("May");
-        } else if (month == 6) {
-            System.out.println("June");
-        } else if (month == 7) {
-            System.out.println("July");
-        } else if (month == 8) {
-            System.out.println("August");
-        } else if (month == 9) {
-            System.out.println("September");
-        } else if (month == 10) {
-            System.out.println("October");
-        } else if (month == 11) {
-            System.out.println("November");
-        } else if (month == 12) {
-            System.out.println("December");
-        } else {
-            System.out.println("wrong number!");
-        }
+         switch (month) {
+                case 1:
+                    System.out.println(31);
+                    break;
+                case 2:
+                    System.out.println(28);
+                    break;
+                case 3:
+                    System.out.println(31);
+                    break;
+                case 4:
+                    System.out.println(30);
+                    break;
+                case 5:
+                    System.out.println(31);
+                    break;
+                case 6:
+                    System.out.println(30);
+                    break;
+                case 7:
+                    System.out.println(31);
+                    break;
+                case 8:
+                    System.out.println(31);
+                    break;
+                case 9:
+                    System.out.println(30);
+                    break;
+                case 10:
+                    System.out.println(31);
+                    break;
+                case 11:
+                    System.out.println(30);
+                    break;
+                case 12:
+                    System.out.println(31);
+                    break;
+                default:
+                    System.out.println("wrong number!");
+            }
     }
 }
